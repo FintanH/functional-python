f9da8a7e73fef4cbb91a0e7fdc52b1a78322f048
fintan <rossmanf@tcd.ie>
2015-03-20 16:23:40 +0000

    Added curry synonym for partial application to function

--- methods.py
+++ methods.py
@@ -1,6 +1,12 @@
 __author__ = 'fintan'
-from functools import reduce
+from functools import reduce, partial
 
 
 def compose(*functions):
     return reduce(lambda f, g: lambda x: f(g(x)), functions)
+
+
+def curry(f, a):
+    return partial(f, a)
+
+
